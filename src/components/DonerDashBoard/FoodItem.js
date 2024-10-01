8aa54ef53faaa474811c856231b334a761677aa2
Youssefnabil88 <youssefwaer145@gmail.com>
2024-10-01 19:58:16 +0300

    Extract a FoodItem into a seprate component

--- src/components/DonerDashBoard/FoodItem.js
+++ src/components/DonerDashBoard/FoodItem.js
@@ -1,5 +1,12 @@
-export default function FoodItem({food, index}){
-    return( <div className="g-col-6" style={{ width: "18rem" }}>
-        
-      </div>)
+export default function FoodItem({ food, index }) {
+  return (
+    <div className="col-12 col-sm-6 col-md-4 mb-4" key={index}>
+      <div className="card h-100">
+        <div className="card-body">
+          <h5 className="card-title">{food.name}</h5>
+          <p className="card-text">{food.quantity}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/DonerDashBoard/FoodList.js
+++ src/components/DonerDashBoard/FoodList.js
@@ -1,18 +1,12 @@
 import React from 'react';
+import FoodItem from './FoodItem';
 
 
 const FoodList = ({ foodList }) => {
     return (
         <div className="row">
             {foodList.map((food, index) => (
-                <div className="col-12 col-sm-6 col-md-4 mb-4" key={index}>
-                    <div className="card h-100"> 
-                        <div className="card-body">
-                            <h5 className="card-title">{food.name}</h5>
-                            <p className="card-text">{food.quantity}</p>
-                        </div>
-                    </div>
-                </div>
+                <FoodItem food={food} index={index}/>
             ))}
         </div>
     );
